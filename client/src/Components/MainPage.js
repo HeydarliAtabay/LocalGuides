5047f2d38541e77de5b355dde0ecc74719107d00
ElchinFarhad <elchin.ferhadov@gmail.com>
2021-02-13 22:09:08 +0100

    feat: autofill google api added

--- client/src/Components/MainPage.js
+++ client/src/Components/MainPage.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 import {Card, Image, Form, Button} from 'react-bootstrap';
 import './CSS/MainPage.css'
+import Search from './Search';
+import Script from 'react-load-script';
 
 class MainPage extends Component {
+
+
   constructor(props) {
     super(props);
-    this.setLoc = this.setLoc.bind(this);
-    this.callLoc = this.callLoc.bind(this);
+    // this.setLoc = this.setLoc.bind(this);
+    // this.callLoc = this.callLoc.bind(this);
     this.state = {
       location: ""
     }
   }
+  componentDidMount(){
+    this.callLoc();
+  }
   setLoc=(e)=>{
-    this.setState({location: e})
+    this.setState({location: e});
+    alert(e);
+
      }
+
+  
 
   callLoc=()=>{
     if (navigator.geolocation) {
@@ -28,33 +39,36 @@ class MainPage extends Component {
         xhr.send(); 
         xhr.onreadystatechange = processRequest; 
         xhr.addEventListener("readystatechange", processRequest, false); 
-      
+    
         function processRequest(e) { 
             if (xhr.readyState == 4 && xhr.status == 200) { 
                 var response = JSON.parse(xhr.responseText); 
                 var city = response.address.city; 
                 var country = response.address.country; 
                 var res = city.concat(", ", country);
-                // this.setLoc(res);
-                alert(res); 
-                return; 
+                window.t=res;
+                return res; 
             } 
         }
     }
+    this.setLoc(window.t);
   }
 
   
   render() {
     return (
       <div id="main">
+        <Script url="https://maps.googleapis.com/maps/api/js?                               key=your_api_key&libraries=places"          
+      onLoad={this.handleScriptLoad}        />
+         
         <h3 id="mainText">Choose Your Next Destination</h3>
         <div id='card'>
       <div class="input-group">
-          <input placeholder="City name" type='text'></input>
+          <Search location={this.state.location}/>
           <Button onClick={this.callLoc}>Search</Button>
           </div>
           <br></br>
-          <Button onClick={this.callLoc} block>Near by Guides</Button>
+          <Button onClick={()=>this.callLoc()} block>Near by Guides</Button>
         </div>
 
       </div>
